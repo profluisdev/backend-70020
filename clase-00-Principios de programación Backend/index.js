118d056e189ea987c0d56dadb0adbf96a94633d7
Luis Mera <profeluismeradev@gmail.com>
2024-05-14 11:36:02 -0300

    clase 0

---
+++ clase-00-Principios de programación Backend/index.js
@@ -0,0 +1,12 @@
+
+console.log("Hola");
+console.warn("Alerta");
+console.error("Error");
+// console.clear();
+
+let name = "Pepe";
+
+console.log(name);
+
+let array = ["Banana", 1, true];
+console.log(array);
